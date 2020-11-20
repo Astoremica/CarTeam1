f6f07cef7f672dd616238e50dd7aa1da76bba815
yukiii0529 <57751716+yukiii0529@users.noreply.github.com>
2020-11-20 12:36:04 +0900

    header修正

--- CarTeam1/resources/views/home.blade.php
+++ CarTeam1/resources/views/home.blade.php
@@ -1,12 +1,9 @@
 @extends('layouts.layout')
+<!-- head -->
+@include('common.head')
 
 <!-- header -->
 @include('common.header')
-<link rel="stylesheet" href="{{asset('css/app.css')}}" />
-<link rel="stylesheet" href="{{asset('css/layout/header.css')}}" />
-<link rel="stylesheet" href="{{asset('css/layout/footer.css')}}" />
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
 
 @section('content')
 <div class="container">
