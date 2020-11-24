28bbd68d17da80edd22cadd9622820e781337e82
matsueda2776 <65533665+matsueda2776@users.noreply.github.com>
2020-11-24 14:49:38 +0900

    アクセサリー

--- CarTeam1/database/seeds/ControllersTableSeeder.php
+++ CarTeam1/database/seeds/ControllersTableSeeder.php
@@ -22,6 +22,53 @@ class ControllersTableSeeder extends Seeder
                 'controller'    => 'エアコン',
                 'created_at' => new DateTime(),
             ],
+            [
+                'CARNO' => 'Z12-123456',
+                'controller'    => '純正AW',
+                'created_at' => new DateTime(),
+            ],
+            [
+                'CARNO' => 'Z12-121111',
+                'controller'    => 'エアコン',
+                'created_at' => new DateTime(),
+            ],
+            [
+                'CARNO' => 'Z12-121111',
+                'controller'    => '純正ナビ',
+                'created_at' => new DateTime(),
+            ],
+            [
+                'CARNO' => 'Z12-121111',
+                'controller'    => '純正エアロ',
+                'created_at' => new DateTime(),
+            ],
+            [
+                'CARNO' => 'GGD2-111456',
+                'controller'    => 'エアコン',
+                'created_at' => new DateTime(),
+            ],
+            [
+                'CARNO' => 'G-332-22',
+                'controller'    => 'エアコン',
+                'created_at' => new DateTime(),
+            ],
+            [
+                'CARNO' => 'G-332-22',
+                'controller'    => '本革シート',
+                'created_at' => new DateTime(),
+            ],
+            [
+                'CARNO' => 'EEQs-212k22',
+                'controller'    => '本革シート',
+                'created_at' => new DateTime(),
+            ],
+            [
+                'CARNO' => 'EEQs-212k22',
+                'controller'    => '純正ナビ',
+                'created_at' => new DateTime(),
+            ],
+
+
         ]);
     }
 }
