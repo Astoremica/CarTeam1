bf7ec762677c89df6c163ecf09a46175c769a9f2
yukiii0529 <57751716+yukiii0529@users.noreply.github.com>
2021-01-14 11:47:07 +0900

    car_detail
    車両詳細

--- CarTeam1/resources/views/user/car.blade.php
+++ CarTeam1/resources/views/user/car.blade.php
@@ -1,58 +1,62 @@
 @extends('layouts.layout')
 <!-- head -->
 @include('common.head')
+<link rel="stylesheet" href="{{asset('css/layout/car.css')}}" />
 
 <!-- header -->
 @include('common.header')
 
 @section('content')
 <div class="container">
+
+  <a href="javascript:history.back()" class="btn return-button">〈 前に戻る</a>
+
   @isset($car)
   <div class="d-flex">
     <div class="item-left">
-      <img src="{{ asset('img/cars/' . $car['CARNO'] . '_1.jpg') }}" width="550px">
+      <img src="{{ asset('img/cars/' . $car['CARNO'] . '_1.jpg') }}" width="565px" class="main-img">
       <div class="d-flex">
         @for($i = 1 ; $i <= $car['IMGSU'] ; $i++)
-          <img src="{{ asset('img/cars/' . $car['CARNO'] . '_' . $i . '.jpg') }}" width="100px">
+          <img src="{{ asset('img/cars/' . $car['CARNO'] . '_' . $i . '.jpg') }}" width="90px" class="sub-img">
         @endfor
       </div>
     </div>
     <div class="item-right">
-      <div class="column">
-        <p>開催日時</p>
-        <p>{{ $car['STRDT'] }} 〜</p>
+      <div class="column-date">
+        <h6>開催日時</h6>
+        <h2>{{ $car['STRDT'] }} 〜</h2>
       </div>
-      <div class="column">
-        <p>オークション開始価格</p>
-        <p>{{ number_format($car['STRPR'] * 1000) }}円</p>
+      <div class="column-price">
+        <h6>オークション開始価格</h6>
+        <h2>{{ number_format($car['STRPR'] * 1000) }}<span class="price">円</span></h2>
       </div>
       <div class="d-flex">
-        <div class="column">
-          <p>年式</p>
-          <p>{{ substr($car['NENSK'],0,2) }}年{{ substr($car['NENSK'],2,2) }}月</p>
+        <div class="column-some">
+          <p><u>年式</u></p>
+          <p class="display">{{ substr($car['NENSK'],0,2) }}年<br>{{ substr($car['NENSK'],2,2) }}月</p>
         </div>
-        <div class="column">
-          <p>走行距離</p>
-          <p>{{ number_format($car['SOUKM']) }}km</p>
+        <div class="column-some">
+          <p><u>走行距離</u></p>
+          <p class="display">{{ number_format($car['SOUKM']) }}<br>km</p>
         </div>
-        <div class="column">
-          <p>車検期限</p>
-          <p>{{ substr($car['KENKG'], 0, 4) }}年{{ substr($car['KENKG'], 4, 2) }}月{{ substr($car['KENKG'], 6, 2) }}日</p>
+        <div class="column-some">
+          <p><u>車検期限</u></p>
+          <p class="display">{{ substr($car['KENKG'], 0, 4) }}年<br>{{ substr($car['KENKG'], 4, 2) }}月{{ substr($car['KENKG'], 6, 2) }}日</p>
         </div>
-        <div class="column">
-          <p>修復歴</p>
-          @if(!$car['SYURK'])
-            <p>なし</p>
+        <div class="column-some">
+          <p><u>修復歴</u></p>
+          @if($car['SYURK'] == 0)
+            <p class="display">無し</p>
           @else
-            <p>{{ $car['SYURK'] }}回</p>
+            <p class="display">有り</p>
           @endif
         </div>
       </div>
     </div>
   </div>
 
-  <div>
-    <p>車両情報</p>
+  <div class="car-info">
+    <h3 class="column-title">車両情報</h3>
     <table class="table table-bordered">
       <thead class="thead-lignt">
         <tr>
@@ -61,14 +65,14 @@
           <td class="table-active" style="width:13%">燃料</td><td style="width:20%">{{ $car['NENRY'] }}</td>
         </tr>
         <tr>
-          <td class="table-active" style="width:13%">乗員数</td><td style="width:20%">{{ $car['TEIIN'] }}</td>
-          <td class="table-active" style="width:13%">排気量</td><td style="width:20%">{{ $car['HIKRY'] }}</td>
+          <td class="table-active" style="width:13%">乗員数</td><td style="width:20%">{{ $car['TEIIN'] }}人</td>
+          <td class="table-active" style="width:13%">排気量</td><td style="width:20%">{{ number_format($car['HIKRY']) }}cc</td>
           <td class="table-active" style="width:13%">色</td><td style="width:20%">{{ $car['COLOR'] }}</td>
         </tr>
         <tr>
           <td class="table-active" style="width:13%">型式</td><td style="width:20%">{{ $car['KATSK'] }}</td>
-          <td class="table-active" style="width:13%">車検</td><td style="width:20%">{{ $car['KENKG'] }}</td>
-          <td class="table-active" style="width:13%">走行距離</td style="width:20%"><td>{{ $car['SOUKM'] }}</td>
+          <td class="table-active" style="width:13%">車検</td><td style="width:20%">{{ substr($car['KENKG'], 0, 4) }}年{{ substr($car['KENKG'], 4, 2) }}月{{ substr($car['KENKG'], 6, 2) }}日</td>
+          <td class="table-active" style="width:13%">走行距離</td style="width:20%"><td>{{ number_format($car['SOUKM']) }}km</td>
         </tr>
         <tr>
           <td class="table-active" style="width:13%">修復歴</td><td style="width:20%">@if($car['SYURK'] == 0)なし @else 有 @endif</td>
@@ -90,8 +94,10 @@
     </table>
   </div>
 
+  <hr>
+
   <div>
-    <p>展開図・特記事項</p>
+    <h3 class="column-title">展開図・特記事項</h3>
     <div class="d-flex">
       <div class="item-left">
         <div class="card" style="width: 340px; border-radius:20px; padding: 10px 0 10px 15px;">
@@ -115,47 +121,50 @@
             @endforeach
           </tbody>
         </table>
-        <div class="d-flex">
-          <div class="column">
+        <div class="d-flex damage-list">
+          <div class="column-con">
             <h4>A</h4>
-            <p>キズ</p>
+            <p class="damage-det">キズ</p>
           </div>
-          <div class="column">
+          <div class="column-con">
             <h4>U</h4>
-            <p>凹</p>
+            <p class="damage-det">凹</p>
           </div>
-          <div class="column">
+          <div class="column-con">
             <h4>W</h4>
-            <p>補修波</p>
+            <p class="damage-det">補修波</p>
           </div>
-          <div class="column">
+          <div class="column-con">
             <h4>X</h4>
-            <p>要交換</p>
+            <p class="damage-det">要交換</p>
           </div>
-          <div class="column">
+          <div class="column-con">
             <h4>XX</h4>
-            <p>交換済み</p>
+            <p class="damage-det">交換済み</p>
           </div>
-          <div class="column">
+          <div class="column-con">
             <h4>P</h4>
-            <p>要塗装</p>
+            <p class="damage-det">要塗装</p>
           </div>
-          <div class="column">
+          <div class="column-con">
             <h4>S</h4>
-            <p>錆</p>
+            <p class="damage-det">錆</p>
           </div>
-          <div class="column">
+          <div class="column-con">
             <h4>C</h4>
-            <p>腐食</p>
+            <p class="damage-det">腐食</p>
           </div>
-          <div class="column">
+          <div class="column-con">
             <h4>B</h4>
-            <p>傷凹</p>
+            <p class="damage-det">傷凹</p>
           </div>
         </div>
       </div>
     </div>
   </div>
+
+  <a href="javascript:history.back()" class="btn return-button">〈 前に戻る</a>
+ 
 
   @endif
 </div>
