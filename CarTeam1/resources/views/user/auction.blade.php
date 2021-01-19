04798ccbf531e7dd1553cbd43b8438e111c5ad69
Astoremica <misawashi4zy@gmail.com>
2021-01-19 15:09:44 +0900

    change auction

--- CarTeam1/resources/views/user/auction.blade.php
+++ CarTeam1/resources/views/user/auction.blade.php
@@ -259,58 +259,13 @@
                     $('#inputPrice').addClass("endinputprice");
                     $('#enterButton').prop('disabled', true);
                     $('#enterButton').addClass("endbutton");
-
-                    if (flag = true) {
-
-                        var json_asocc = [{
-                            'carno': '{{ $car->CARNO }}'
-                        }];
-
-                        //JSONにエンコード
-                        var json_text = JSON.stringify(json_asocc);
-
-                        //データを送信
-                        xhr = new XMLHttpRequest; //インスタンス作成
-                        xhr.onload = function() { //レスポンスを受け取った時の処理（非同期）
-                            var res = xhr.responseText;
-                            if (res.length > 0) alert(res);
-                        };
-                        xhr.onerror = function() { //エラーが起きた時の処理（非同期）
-                            alert("error!");
-                        }
-                        xhr.open('post', "http://localhost:9000/endauction", true); //(1)
-                        xhr.setRequestHeader('Content-Type', 'application/json');
-                        xhr.send(json_text); //送信実行
-
-
-                        // var data = {
-                        //     carno: '{{ $car->CARNO }}'
-                        // };
-                        // // 通信実行
-                        // $.ajax({
-                        //     type: "post", // method = "POST"
-                        //     url: "http://localhost:9000/endauction", // POST送信先のURL
-                        //     data: JSON.stringify(data), // JSONデータ本体
-                        //     contentType: 'application/json', // リクエストの Content-Type
-                        //     dataType: "json", // レスポンスをJSONとしてパースする
-                        //     success: function(json_data) { // 200 OK時
-                        //         // JSON Arrayの先頭が成功フラグ、失敗の場合2番目がエラーメッセージ
-                        //         if (!json_data[0]) { // サーバが失敗を返した場合
-                        //             alert("Transaction error. " + json_data[1]);
-                        //             return;
-                        //         }
-                        //     },
-                        //     error: function() { // HTTPエラー時
-                        //         alert("Server Error. Pleasy try again later.");
-                        //     },
-                        //     complete: function() { // 成功・失敗に関わらず通信が終了した際の処理
-                        //         button.attr("disabled", false); // ボタンを再び enableにする
-                        //     }
-                        // });
-                    }
                     flg = false;
                 } else {
                     msg = msg2;
+
+                }
+                // 終了した時の処理を入れようかな
+                if (flag == false) {
 
                 }
 
