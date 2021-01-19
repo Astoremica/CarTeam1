b75f126e4cf7f89fd1d43d5a7499c1109cb97d57
せいやーさん <51153166+seiya12@users.noreply.github.com>
2021-01-19 12:58:34 +0900

    change 日本語で遊ぼ

--- CarTeam1/resources/views/admin/home.blade.php
+++ CarTeam1/resources/views/admin/home.blade.php
@@ -12,7 +12,7 @@
             <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">CPU Traffic</span>
+              <span class="info-box-text">サーバーCPU使用率</span>
               <span class="info-box-number">
                 10
                 <small>%</small>
@@ -28,8 +28,8 @@
             <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Likes</span>
-              <span class="info-box-number">41,410</span>
+              <span class="info-box-text">サイト訪問数</span>
+              <span class="info-box-number">368</span>
             </div>
             <!-- /.info-box-content -->
           </div>
@@ -45,7 +45,7 @@
             <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Sales</span>
+              <span class="info-box-text">取引数</span>
               <span class="info-box-number">760</span>
             </div>
             <!-- /.info-box-content -->
@@ -58,7 +58,7 @@
             <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">New Members</span>
+              <span class="info-box-text">ユーザー数</span>
               <span class="info-box-number">2,000</span>
             </div>
             <!-- /.info-box-content -->
@@ -73,7 +73,7 @@
         <div class="col-md-12">
           <div class="card">
             <div class="card-header">
-              <h5 class="card-title">Monthly Recap Report</h5>
+              <h5 class="card-title">売上推移</h5>
 
               <div class="card-tools">
                 <button type="button" class="btn btn-tool" data-card-widget="collapse">
@@ -101,7 +101,7 @@
               <div class="row">
                 <div class="col-md-8">
                   <p class="text-center">
-                    <strong>Sales: 1 Jan, 2014 - 30 Jul, 2014</strong>
+                    <strong>Sales: 1 Jan, 2020 - 30 Jul, 2020</strong>
                   </p>
 
                   <div class="chart">
@@ -117,7 +117,7 @@
                   </p>
 
                   <div class="progress-group">
-                    Add Products to Cart
+                    売上総数
                     <span class="float-right"><b>160</b>/200</span>
                     <div class="progress progress-sm">
                       <div class="progress-bar bg-primary" style="width: 80%"></div>
@@ -126,7 +126,7 @@
                   <!-- /.progress-group -->
 
                   <div class="progress-group">
-                    Complete Purchase
+                    通知送信数
                     <span class="float-right"><b>310</b>/400</span>
                     <div class="progress progress-sm">
                       <div class="progress-bar bg-danger" style="width: 75%"></div>
@@ -135,7 +135,7 @@
 
                   <!-- /.progress-group -->
                   <div class="progress-group">
-                    <span class="progress-text">Visit Premium Page</span>
+                    <span class="progress-text">買取数</span>
                     <span class="float-right"><b>480</b>/800</span>
                     <div class="progress progress-sm">
                       <div class="progress-bar bg-success" style="width: 60%"></div>
@@ -144,7 +144,7 @@
 
                   <!-- /.progress-group -->
                   <div class="progress-group">
-                    Send Inquiries
+                    通知反応数
                     <span class="float-right"><b>250</b>/500</span>
                     <div class="progress progress-sm">
                       <div class="progress-bar bg-warning" style="width: 50%"></div>
@@ -162,8 +162,8 @@
                 <div class="col-sm-3 col-6">
                   <div class="description-block border-right">
                     <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 17%</span>
-                    <h5 class="description-header">$35,210.43</h5>
-                    <span class="description-text">TOTAL REVENUE</span>
+                    <h5 class="description-header">¥35,210</h5>
+                    <span class="description-text">通知打ち上げ</span>
                   </div>
                   <!-- /.description-block -->
                 </div>
@@ -171,8 +171,8 @@
                 <div class="col-sm-3 col-6">
                   <div class="description-block border-right">
                     <span class="description-percentage text-warning"><i class="fas fa-caret-left"></i> 0%</span>
-                    <h5 class="description-header">$10,390.90</h5>
-                    <span class="description-text">TOTAL COST</span>
+                    <h5 class="description-header">¥10,390</h5>
+                    <span class="description-text">総コスト</span>
                   </div>
                   <!-- /.description-block -->
                 </div>
@@ -180,8 +180,8 @@
                 <div class="col-sm-3 col-6">
                   <div class="description-block border-right">
                     <span class="description-percentage text-success"><i class="fas fa-caret-up"></i> 20%</span>
-                    <h5 class="description-header">$24,813.53</h5>
-                    <span class="description-text">TOTAL PROFIT</span>
+                    <h5 class="description-header">¥24,813</h5>
+                    <span class="description-text">催促売り上げ数</span>
                   </div>
                   <!-- /.description-block -->
                 </div>
@@ -190,7 +190,7 @@
                   <div class="description-block">
                     <span class="description-percentage text-danger"><i class="fas fa-caret-down"></i> 18%</span>
                     <h5 class="description-header">1200</h5>
-                    <span class="description-text">GOAL COMPLETIONS</span>
+                    <span class="description-text">ミッション達成数</span>
                   </div>
                   <!-- /.description-block -->
                 </div>
@@ -236,20 +236,20 @@
                 <div class="card-pane-right bg-success pt-2 pb-2 pl-4 pr-4">
                   <div class="description-block mb-4">
                     <div class="sparkbar pad" data-color="#fff">90,70,90,70,75,80,70</div>
-                    <h5 class="description-header">8390</h5>
-                    <span class="description-text">Visits</span>
+                    <h5 class="description-header">8,390</h5>
+                    <span class="description-text">実店舗訪問数</span>
                   </div>
                   <!-- /.description-block -->
                   <div class="description-block mb-4">
                     <div class="sparkbar pad" data-color="#fff">90,50,90,70,61,83,63</div>
                     <h5 class="description-header">30%</h5>
-                    <span class="description-text">Referrals</span>
+                    <span class="description-text">取引成功率</span>
                   </div>
                   <!-- /.description-block -->
                   <div class="description-block">
                     <div class="sparkbar pad" data-color="#fff">90,50,90,70,61,83,63</div>
                     <h5 class="description-header">70%</h5>
-                    <span class="description-text">Organic</span>
+                    <span class="description-text">目標達成率</span>
                   </div>
                   <!-- /.description-block -->
                 </div><!-- /.card-pane-right -->
@@ -267,7 +267,7 @@
             <span class="info-box-icon"><i class="fas fa-tag"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Inventory</span>
+              <span class="info-box-text">タグ付け</span>
               <span class="info-box-number">5,200</span>
             </div>
             <!-- /.info-box-content -->
@@ -277,7 +277,7 @@
             <span class="info-box-icon"><i class="far fa-heart"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Mentions</span>
+              <span class="info-box-text">アクティブ</span>
               <span class="info-box-number">92,050</span>
             </div>
             <!-- /.info-box-content -->
@@ -287,7 +287,7 @@
             <span class="info-box-icon"><i class="fas fa-cloud-download-alt"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Downloads</span>
+              <span class="info-box-text">ダウンロード</span>
               <span class="info-box-number">114,381</span>
             </div>
             <!-- /.info-box-content -->
@@ -297,7 +297,7 @@
             <span class="info-box-icon"><i class="far fa-comment"></i></span>
 
             <div class="info-box-content">
-              <span class="info-box-text">Direct Messages</span>
+              <span class="info-box-text">通知</span>
               <span class="info-box-number">163,921</span>
             </div>
             <!-- /.info-box-content -->
