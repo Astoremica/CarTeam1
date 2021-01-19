3d370e5e8673d55baed07b27e1ec4ba24cfc8a3e
yukiii0529 <57751716+yukiii0529@users.noreply.github.com>
2021-01-19 12:20:31 +0900

    マイページ変更

--- CarTeam1/resources/views/user/mypage_favorite_list.blade.php
+++ CarTeam1/resources/views/user/mypage_favorite_list.blade.php
@@ -15,7 +15,7 @@
         <div class="card-body">
           <h5 class="card-title">名前：{{ $user['name1'] }} {{$user['name2'] }}</h5>
           <h5 class="card-subtitle text-muted nickname">ニックネーム：{{ $user['name3'] }}</h5>
-          <a href="#" class="btn btn-primary button01">プロフィール編集</a>
+          <a href="#" class="btn btn-primary">プロフィール編集</a>
         </div>
       </div>
       <a href="/user/mypage" class="btn button" style="width: 18rem;">マイページTOP</a>
@@ -25,54 +25,6 @@
     </div>
 
     <div class="item-right">
-      <h3 class="title"><img src="{{ asset('img/cars/title-car.png') }}" width="32px" class="title-img">現在取引中の車両</h3>
-      @isset($cars)
-        @if($cars->isEmpty())
-          <p class="null">現在取引中の車両はありません。</p>
-        @else
-          @foreach($cars as $car)
-            @foreach($transactions as $transaction)
-            @if($car['CARNO'] == $transaction['CARNO'])
-            <div class="cars">
-              <div class="d-flex">
-                <?php
-                  $str="";
-                  for($i=0;$i<8;$i++){
-                      $str.=mt_rand(0,9);
-                  }
-                ?>
-                <h4 class="car-title1">取引ID：{{ $str }}</h4>
-                <h4 class="car-title2">落札日：{{ $car['STRDT'] }}</h4>
-                <h4 class="car-title3">落札金額：{{ number_format($transaction['price'] * 1000) }}円</h4>
-              </div>
-              <div class="d-flex detail">
-                <div class="car-item1">
-                  <?php $filename = 'img/cars/' . $car['CARNO'] . '_1.jpg'; ?>
-                  @if(file_exists($filename))
-                      <img src="{{ asset('img/cars/' . $car['CARNO'] . '_1.jpg') }}" alt="メーカー名:車種名" width="180px" class="fav-img"/>
-                  @else
-                      <img src="{{ asset('img/cars/car.png') }}" alt="メーカー名:車種名" width="155px" class="null-img"/>
-                  @endif
-                </div>
-                <div class="car-item2">
-                  <h3>{{ $car['CARNM'] }}</h3>
-                  @if($transaction['k_status'] == 1 && $transaction['n_status'] == 1)
-                    <h4>取引ステータス：取引終了</h4>
-                  @else
-                    <h4>取引ステータス：提出書類確認中</h4>
-                  @endif
-                </div>
-                <div class="car-item3">
-                  <a href="/user/cars/{{ $car['CARNO'] }}" class="btn detail-button" style="width: 13rem;">車両詳細</a>
-                </div>
-              </div>
-            </div>
-            @endif
-            @endforeach
-          @endforeach
-        @endif
-      @endif
-
       <h3 class="title"><img src="{{ asset('img/cars/title-car.png') }}" width="32px" class="title-img">お気に入り登録した車両</h3>
       @isset($favorites)
         @if($favorites->isEmpty())
@@ -128,6 +80,7 @@
         @endif
       @else
       @endif
+      <a href="javascript:history.back()" class="btn return-button">〈 前に戻る</a>
     </div>
   </div>
 </div>
